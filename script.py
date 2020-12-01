d6a3cc9f8d89f05d093fad5b3c40a12bf51337c8
nildiert <niljordan23@gmail.com>
2020-11-30 21:51:27 -0500

    [UPDATE]

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import pandas as pd
 
+count = 0
 with open('data.txt') as reader:
     data = reader.read()
     names = data.split('\n')[2:-1]
@@ -10,6 +11,8 @@ with open('data.txt') as reader:
         if file.endswith('.xlsx'):
             file = 'data/' + file
             df_excel = pd.read_excel(file)
+            count += df_excel.shape[0]
             df = df.append(pd.read_excel(file), ignore_index=True)
 
+print(count, "elements")
 df.to_excel('combined_file.xlsx')
